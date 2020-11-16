c1fdac68e6feaf43690c834e1922ea656c126a5d
Cody Merritt <cody.merritt@me.com>
2020-11-16 15:12:17 -0500

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+
+class Project
+
+    attr_reader :title
+    attr_accessor :backers
+    @@all = []
+  
+    def initialize(title)
+      @title = title
+      @@all << self
+      @backers = []
+    end
+  
+    def add_backer(backer)
+        @backers << backer
+        backer.projects << self
+        ProjectBacker.new(self, backer)
+    end
+  
+  end
